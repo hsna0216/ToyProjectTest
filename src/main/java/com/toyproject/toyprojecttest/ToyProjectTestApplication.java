45103db1973ad05e8fa2b7be8eb28e40808dd045
현서 <hyunseo@hyunseos-MacBook-Air.local>
2022-04-12 00:48:30 +0900

    Initial commit

---
+++ src/main/java/com/toyproject/toyprojecttest/ToyProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.toyprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProjectTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproject/toyprojecttest/ToyProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.toyprojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
